e352753d1b9d7c5e588ee760b29cee6b5a429636
Aldiney Moreira <aldiney.mr@gmail.com>
2023-11-09 14:54:01 -0300

    feat: show total, subtotal and total discount on cart

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -4,9 +4,11 @@ import { CartContext } from "@/providers/cart";
 import { useContext } from "react";
 import CartItem from "./cart-item";
 import { computeProductTotalPrice } from "@/helpers/product";
+import { Separator } from "./separator";
+import { Button } from "./button";
 
 const Cart = () => {
-  const { products } = useContext(CartContext);
+  const { products, subtotal, totalDiscount, total } = useContext(CartContext);
   return (
     <div className="flex flex-col gap-5">
       <Badge
@@ -14,7 +16,7 @@ const Cart = () => {
         variant="outline"
       >
         <ShoppingCartIcon size={16} />
-        Catálogo
+        Carrinho
       </Badge>
 
       <div className="flex h-full flex-col gap-5">
@@ -31,6 +33,38 @@ const Cart = () => {
           </p>
         )}
       </div>
+
+      {products.length > 0 && (
+        <div className="flex flex-col gap-3">
+          <Separator />
+
+          <div className="flex items-center justify-between text-xs lg:text-sm">
+            <p>Subtotal</p>
+            <p>R$ {subtotal.toFixed(2)}</p>
+          </div>
+
+          <Separator />
+
+          <div className="flex items-center justify-between text-xs lg:text-sm">
+            <p>Entrega</p>
+            <p>GRÁTIS</p>
+          </div>
+
+          <Separator />
+
+          <div className="flex items-center justify-between text-xs lg:text-sm">
+            <p>Descontos</p>
+            <p>- R$ {totalDiscount.toFixed(2)}</p>
+          </div>
+
+          <Separator />
+
+          <div className="flex items-center justify-between text-sm font-bold lg:text-base">
+            <p>Total</p>
+            <p>R$ {total.toFixed(2)}</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
